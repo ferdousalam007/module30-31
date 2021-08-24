50f85d2ba52c2d8eed1682f0e51f68fe78ed270e
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-24 14:32:47 +0600

    more map

--- DestructuringObject.js
+++ DestructuringObject.js
@@ -1,1 +1,26 @@
-console.log(111)
+const fish = { id: 58, name: 'King Hilsha', price: 9000, phone: '01717555555555', address: 'Chandpur', dress: 'silver' };
+const {name,id,price} = fish;
+console.log(name,id,price);
+
+
+const company = {
+    name: 'GP',
+    ceo: { id: 1, name: 'ajmol', food: 'fuchka' },
+    web: {
+        work: 'website development',
+        employee: 22,
+        framework: 'react',
+        tech: {
+            first: 'html',
+            second: 'css',
+            third: 'js'
+        }
+    },
+};
+
+// const work = company.web.work;
+// const framework = company.web.framework;
+const { work, framework } = company.web;
+const { food } = company.ceo;
+const { second, third } = company.web.tech;
+console.log(work, framework, food);
